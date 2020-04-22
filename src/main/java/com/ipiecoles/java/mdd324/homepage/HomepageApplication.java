9529714167f5bbdf060d6f172870eee5f9884f18
Shayne <khaldi.alternance@gmail.com>
2020-04-22 15:19:37 +0200

    AWS FINALISER

--- src/main/java/com/ipiecoles/java/mdd324/homepage/HomepageApplication.java
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/HomepageApplication.java
@@ -1,23 +1,7 @@
 package com.ipiecoles.java.mdd324.homepage;
 
-import com.ipiecoles.java.mdd324.homepage.model.Rss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import javax.xml.bind.JAXB;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import static jdk.nashorn.internal.objects.Global.println;
 
 @SpringBootApplication
 public class HomepageApplication {
